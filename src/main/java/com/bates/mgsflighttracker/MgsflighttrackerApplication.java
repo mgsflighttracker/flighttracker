b6355258b4bfdddde505e252ec6d366cedd37694
Bates <GDB0204@daugherty.com>
2021-10-10 14:17:27 -0500

    Initial Commit

---
+++ src/main/java/com/bates/mgsflighttracker/MgsflighttrackerApplication.java
@@ -0,0 +1,13 @@
+package com.bates.mgsflighttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgsflighttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgsflighttrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bates/mgsflighttracker/MgsflighttrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bates.mgsflighttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgsflighttrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
